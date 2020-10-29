00ec1b78a6b3bad3fc6d8322641cce5a99da3344
svetlana-rezvaya <svetlana-2305@mail.ru>
2020-10-29 21:14:54 +0300

    Count characters

--- main.go
+++ main.go
@@ -6,11 +6,13 @@ import (
 	"io"
 	"log"
 	"os"
+	"unicode/utf8"
 )
 
 func main() {
 	lineCount := 0
 	byteCount := 0
+	characterCount := 0
 	reader := bufio.NewReader(os.Stdin)
 	for {
 		line, err := reader.ReadString('\n')
@@ -27,10 +29,12 @@ func main() {
 
 		lineCount = lineCount + 1
 		byteCount = byteCount + len(line)
+		characterCount = characterCount + utf8.RuneCountInString(line)
 
 		fmt.Print("#", line)
 	}
 
 	fmt.Println("amount of lines =", lineCount)
 	fmt.Println("amount of bytes =", byteCount)
+	fmt.Println("amount of characters =", characterCount)
 }
