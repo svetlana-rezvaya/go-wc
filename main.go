414eda93f0b3fda921a33cd54e28d4a58c0e41ec
svetlana-rezvaya <svetlana-2305@mail.ru>
2020-11-02 21:19:36 +0300

    Add countWords function

--- main.go
+++ main.go
@@ -9,8 +9,21 @@ import (
 	"unicode/utf8"
 )
 
+func countWords(text string) int {
+	spaceCount := 0
+	for _, character := range text {
+		if character == ' ' {
+			spaceCount = spaceCount + 1
+		}
+	}
+
+	wordCount := spaceCount + 1
+	return wordCount
+}
+
 func main() {
 	lineCount := 0
+	wordCount := 0
 	byteCount := 0
 	characterCount := 0
 	reader := bufio.NewReader(os.Stdin)
@@ -28,6 +41,7 @@ func main() {
 		}
 
 		lineCount = lineCount + 1
+		wordCount = wordCount + countWords(line)
 		byteCount = byteCount + len(line)
 		characterCount = characterCount + utf8.RuneCountInString(line)
 
@@ -35,6 +49,7 @@ func main() {
 	}
 
 	fmt.Println("amount of lines =", lineCount)
+	fmt.Println("amount of words =", wordCount)
 	fmt.Println("amount of bytes =", byteCount)
 	fmt.Println("amount of characters =", characterCount)
 }
